4b700177227dd4566f61e3faaf857083aa9e1700
Jordan Aceto <ja532@cornell.edu>
2024-06-14 13:19:49 -0400

    Ignore build artifacts, delete old build dir that snuck in

--- sd_mux_control/build/backup_max32665.d
+++
@@ -1,2 +0,0 @@
-C:/Users/jorda/eclipse-workspace/sd_mux_control/build/backup_max32665.o: \
- C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Source/GCC/backup_max32665.S
--- sd_mux_control/build/startup_max32665.d
+++
@@ -1,2 +0,0 @@
-C:/Users/jorda/eclipse-workspace/sd_mux_control/build/startup_max32665.o: \
- C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Source/GCC/startup_max32665.S

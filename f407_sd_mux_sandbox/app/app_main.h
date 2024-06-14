8e0d36803d429e4fdffdcb820a3f811e50d3e51c
Jordan Aceto <ja532@cornell.edu>
2024-06-14 13:09:31 -0400

    Delete STM32 SD MUX code, add MAX32666 version of SD MUX code

--- f407_sd_mux_sandbox/app/app_main.h
+++
@@ -1,9 +0,0 @@
-#ifndef APP_MAIN_H_
-#define APP_MAIN_H_
-
-/**
- * @brief User application main entry point. Enters an infinite loop and never returns
- */
-void app_main(void);
-
-#endif /* APP_MAIN_H_ */

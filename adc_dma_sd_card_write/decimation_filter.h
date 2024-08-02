03b9d690e8285b74f533715e29b1a00829ed4adf
Jordan Aceto <ja532@cornell.edu>
2024-08-02 15:55:35 -0400

    Update deci filter h comments to reflect reality

--- adc_dma_sd_card_write/decimation_filter.h
+++ adc_dma_sd_card_write/decimation_filter.h
@@ -2,7 +2,7 @@
  * @file      decimation_filters.h
  * @brief     A software interface for decimation filters is represented here.
  * @details   This module is used to down-sample the raw 384kHz 24-bit audio data from the ADC/DMA modules to other
- *            sample rates. It can produce 24-bit or 16-bit results.
+ *            sample rates.
  */
 
 #ifndef DECIMATION_FILTER_H_
@@ -18,8 +18,8 @@
 
 /**
  * `decimation_filter_set_sample_rate(sr)` sets the sample rate for the decimation filter to `sr`. This must not be
- * called in the middle of writing a WAV file. Only call this between SD card file writes when you want to change sample
- * rates.
+ * called in the middle of writing a WAV file. Only call this between SD card file writes when you want to change
+ * sample rates.
  *
  * @param sample_rate the enumerated sample rate to use for following calls to `decimation_filter_downsample()`. Can be
  * any enumerated sample rate EXCEPT for 384kHz, do not call with this sample rate.
@@ -33,16 +33,17 @@ void decimation_filter_set_sample_rate(Wave_Header_Sample_Rate_t sample_rate);
  * @brief `decimation_filter_downsample(s, d, n)` downsamples `n` samples from source buffer `s` and stores the result
  * in destination buffer `d` with the sample rate previously set by `decimation_filter_set_sample_rate(sr)`
  *
- * @pre `decimation_filter_set_sample_rate(sr)` has been called wiht the desired sample rate `sr`
+ * @pre `decimation_filter_set_sample_rate(sr)` has been called with the desired sample rate `sr`
  *
  * @param src_384kHz the source buffer to downsample. It is expected that this is the 384kHz buffer with little-endian
- * q31 samples from the ADC filled via DMA. Must be at least as long as `num_samps_to_filter`
+ * q31 samples. Must be at least as long as `num_samps_to_filter`
  *
  * @param dest the destination buffer for the downsampled data. Must be long enough to hold the downsampled data. The
  * size needed for the destination buffer is given by `num_samps_to_filter * (sr / 384e3)` where `sr` is the previously
  * set sample rate.
  *
- * @param num_samps_to_filter the number of samples from `src` to decimate and store in `dest`. This must be a multiple of 48.
+ * @param num_samps_to_filter the number of samples from `src` to decimate and store in `dest`. This must be a multiple
+ * of 16.
  *
  * @post the source buffer is downsampled and stored in the destination buffer.
  *
